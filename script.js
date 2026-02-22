a37fd15ea9b5c827fde5d2cd7dae94c2355f11bd
arnavshirodkar9-netizen <arnavshirodkar9@gmail.com>
2026-02-22 16:35:13 +0530

    Update script.js

--- script.js
+++ script.js
@@ -18,28 +18,7 @@ document.querySelectorAll("a, button, .card").forEach(el => {
   
 });
 
-document.querySelectorAll(".carousel").forEach(carousel => {
 
-  const track = carousel.querySelector(".carousel-track");
-  const prevBtn = carousel.querySelector(".prev");
-  const nextBtn = carousel.querySelector(".next");
 
-  let index = 0;
-  const images = track.querySelectorAll("img");
-  const totalImages = images.length;
-
-  function updateCarousel() {
-    track.style.transform = `translateX(-${index * 100}%)`;
-  }
-
-  nextBtn.addEventListener("click", () => {
-    index = (index + 1) % totalImages;
-    updateCarousel();
-  });
-
-  prevBtn.addEventListener("click", () => {
-    index = (index - 1 + totalImages) % totalImages;
-    updateCarousel();
-  });
 
 
