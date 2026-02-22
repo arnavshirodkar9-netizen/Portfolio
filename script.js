4990a25f30952653ee2d64368a27b5565e4f60e6
arnavshirodkar9-netizen <arnavshirodkar9@gmail.com>
2026-02-22 16:43:42 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 
-});
-
+// CUSTOM CURSOR
 const cursor = document.querySelector(".cursor");
 
 document.addEventListener("mousemove", (e) => {
@@ -8,14 +7,32 @@ document.addEventListener("mousemove", (e) => {
   cursor.style.top = e.clientY + "px";
 });
 
-document.querySelectorAll("a, button, .card").forEach(el => {
-  el.addEventListener("mouseenter", () => {
-    cursor.style.transform = "translate(-50%, -50%) scale(2)";
+
+// CAROUSEL
+document.querySelectorAll(".carousel").forEach(carousel => {
+
+  const track = carousel.querySelector(".carousel-track");
+  const prevBtn = carousel.querySelector(".prev");
+  const nextBtn = carousel.querySelector(".next");
+
+  let index = 0;
+  const images = track.querySelectorAll("img");
+  const totalImages = images.length;
+
+  function updateCarousel() {
+    track.style.transform = `translateX(-${index * 100}%)`;
+  }
+
+  nextBtn.addEventListener("click", () => {
+    index = (index + 1) % totalImages;
+    updateCarousel();
   });
-  el.addEventListener("mouseleave", () => {
-    cursor.style.transform = "translate(-50%, -50%) scale(1)";
+
+  prevBtn.addEventListener("click", () => {
+    index = (index - 1 + totalImages) % totalImages;
+    updateCarousel();
   });
-  
+
 });
 
 
