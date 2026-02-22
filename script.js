f0584e5a8be89aceddbc91aa6fda10acc855c53f
arnavshirodkar9-netizen <arnavshirodkar9@gmail.com>
2026-02-22 16:29:44 +0530

    Update script.js

--- script.js
+++ script.js
@@ -23,3 +23,22 @@ document.querySelectorAll(".carousel").forEach(carousel => {
   });
 
 });
+
+const cursor = document.querySelector(".cursor");
+
+document.addEventListener("mousemove", (e) => {
+  cursor.style.left = e.clientX + "px";
+  cursor.style.top = e.clientY + "px";
+});
+
+document.querySelectorAll("a, button, .card").forEach(el => {
+  el.addEventListener("mouseenter", () => {
+    cursor.style.transform = "translate(-50%, -50%) scale(2)";
+  });
+  el.addEventListener("mouseleave", () => {
+    cursor.style.transform = "translate(-50%, -50%) scale(1)";
+  });
+});
+
+
+
